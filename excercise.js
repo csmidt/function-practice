cc1bb200cec5973ecbf3fa095a7cae9cc2165a50
Carrie Smidt <carriesmidt@Carries-MacBook-Pro.local>
2016-10-04 11:49:17 -0700

    open

---
+++ excercise.js
@@ -0,0 +1,11 @@
+var greet = function(name) {
+	return "Hello, " + name +"!"
+}
+
+greet(Carrie)
+
+function madlib(adverb, adjective, verb, noun) {
+	return "The " + " " + adverb + " " + adjective +" fox " + verb +" over the " + noun
+}
+
+madlib("swift","happy","sleeps","shoe")
